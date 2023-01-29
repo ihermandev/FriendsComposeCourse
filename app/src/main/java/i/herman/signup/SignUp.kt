ca2be1ce60b3791845602ef416765a67c92fa6f4
illia.herman <illiaherman8@gmail.com>
2023-01-29 13:23:14 +0100

    sign up screen fields interactions

--- app/src/main/java/i/herman/signup/SignUp.kt
+++ app/src/main/java/i/herman/signup/SignUp.kt
@@ -1,13 +1,15 @@
 package i.herman.signup
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.typography
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,32 +24,22 @@ fun SignUp() {
             .padding(16.dp)
     ) {
 
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = stringResource(id = R.string.createAnAccount),
-                style = typography.h4
-            )
-        }
+        ScreenTitle(R.string.createAnAccount)
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = "",
-            onValueChange = {},
-            label = {
-                Text(text = stringResource(id = R.string.email))
+        var email by remember { mutableStateOf("") }
+        EmailField(
+            value = email,
+            onValueChange = {
+                email = it
             })
 
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = "",
-            onValueChange = {},
-            label = {
-                Text(text = stringResource(id = R.string.password))
+        var password by remember { mutableStateOf("") }
+        PasswordField(
+            value = password,
+            onValueChange = {
+                password = it
             })
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -58,5 +50,66 @@ fun SignUp() {
             Text(text = stringResource(id = R.string.signUp))
         }
 
+    }
+}
+
+@Composable
+private fun PasswordField(
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    var isVisible by remember {
+        mutableStateOf(false)
+    }
+
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = value,
+        trailingIcon = {
+            IconButton(
+                onClick = { isVisible = !isVisible }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_visibility_eye),
+                    contentDescription = stringResource(id = R.string.toggleVisibility))
+            }
+        },
+        visualTransformation = if (isVisible) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        onValueChange = onValueChange,
+        label = {
+            Text(text = stringResource(id = R.string.password))
+        })
+}
+
+@Composable
+private fun EmailField(
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = value,
+        onValueChange = onValueChange,
+        label = {
+            Text(text = stringResource(id = R.string.email))
+        })
+}
+
+@Composable
+private fun ScreenTitle(
+    @StringRes titleId: Int,
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(id = titleId),
+            style = typography.h4
+        )
     }
 }
