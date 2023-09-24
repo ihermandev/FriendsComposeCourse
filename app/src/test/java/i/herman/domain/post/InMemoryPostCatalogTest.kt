b49c7fa108ef987f9b63fe2947871d6005b94233
illia.herman <illiaherman8@gmail.com>
2023-09-25 00:28:09 +0200

    introduce InMemoryPostCatalogTest

---
+++ app/src/test/java/i/herman/domain/post/InMemoryPostCatalogTest.kt
@@ -0,0 +1,35 @@
+package i.herman.domain.post
+
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class InMemoryPostCatalogTest {
+
+    private val tomId = "tomId"
+    private val tomPost = Post("tomPostId", tomId, "Post by Tom", 1L)
+    private val annaPost = Post("annaPostId", "annaId", "Post by Anna", 2L)
+    private val lucyPost = Post("lucyPostId", "lucyId", "Post by Lucy", 3L)
+
+    @Test
+    fun postFound() = runBlocking {
+        val postCatalog = InMemoryPostCatalog(
+            availablePosts = listOf(annaPost, tomPost, lucyPost)
+        )
+
+        val result = postCatalog.postsFor(listOf(tomId))
+
+        assertEquals(listOf(tomPost), result)
+    }
+
+    @Test
+    fun postNotFound() = runBlocking {
+        val postCatalog = InMemoryPostCatalog(
+            availablePosts = listOf(annaPost, lucyPost)
+        )
+
+        val result = postCatalog.postsFor(listOf(tomId))
+
+        assertEquals(emptyList<Post>(), result)
+    }
+}
