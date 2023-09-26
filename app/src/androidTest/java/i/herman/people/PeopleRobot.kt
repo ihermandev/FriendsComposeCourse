3b24aa96bfbf67ea58e6bfef28a0e5368bd5d01b
illia.herman <illiaherman8@gmail.com>
2023-09-26 16:09:15 +0200

    people base test

---
+++ app/src/androidTest/java/i/herman/people/PeopleRobot.kt
@@ -0,0 +1,47 @@
+package i.herman.people
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import i.herman.MainActivity
+import i.herman.timeline.launchTimelineFor
+import i.herman.R
+
+private typealias MainActivityRule = AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>
+
+fun launchPeopleFor(
+    email: String,
+    rule: MainActivityRule,
+    block: PeopleRobot.() -> Unit
+): PeopleRobot {
+    launchTimelineFor(email, "Pas$123.", rule) {}
+    return PeopleRobot(rule).apply(block)
+}
+
+class PeopleRobot(private val rule: MainActivityRule) {
+
+    fun tapOnPeople() {
+        val people = rule.activity.getString(R.string.people)
+        rule.onNodeWithText(people)
+            .performClick()
+    }
+
+    infix fun verify(
+        block: PeopleVerificationRobot.() -> Unit
+    ): PeopleVerificationRobot {
+        return PeopleVerificationRobot(rule).apply(block)
+    }
+}
+
+class PeopleVerificationRobot(
+    private val rule: MainActivityRule
+) {
+
+    fun peopleScreenIsPresent() {
+        val people = rule.activity.getString(R.string.people)
+        rule.onNodeWithText(people)
+            .assertIsDisplayed()
+    }
+}
