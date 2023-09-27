a63713b9490afe6e23d5c71bcc92e9fc97a120b6
illia.herman <illiaherman8@gmail.com>
2023-09-27 16:05:51 +0200

    improve timeline state

--- app/src/main/java/i/herman/timeline/state/TimelineScreenState.kt
+++ app/src/main/java/i/herman/timeline/state/TimelineScreenState.kt
@@ -1,38 +1,14 @@
 package i.herman.timeline.state
 
+import android.os.Parcelable
 import androidx.annotation.StringRes
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import i.herman.domain.post.Post
+import kotlinx.parcelize.Parcelize
 
-class TimelineScreenState {
-    var posts by mutableStateOf(emptyList<Post>())
-    var loadedUserId by mutableStateOf("")
-    var isLoading by mutableStateOf(false)
-    var currentInfoMessage by mutableStateOf(0)
 
-    fun updatePosts(newPosts: List<Post>) {
-        isLoading = false
-        this.posts = newPosts
-    }
-
-    fun shouldLoadPostsFor(userId: String): Boolean {
-        if (loadedUserId != userId) {
-            loadedUserId = userId
-            return true
-        }
-        return false
-    }
-
-    fun showLoading() {
-        isLoading = true
-    }
-
-    fun showInfoMessage(@StringRes infoMessage: Int) {
-        isLoading = false
-        if (currentInfoMessage != infoMessage) {
-            currentInfoMessage = infoMessage
-        }
-    }
-}
+@Parcelize
+data class TimelineScreenState(
+    val isLoading: Boolean = false,
+    val posts: List<Post> = emptyList(),
+    @StringRes val error: Int = 0
+) : Parcelable
